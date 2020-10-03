b9c9b42774d4c726c8fa67980de977a5ce36abd4
Thiago Lins <thiagojmlins@gmail.com>
2020-10-03 13:22:51 -0300

    tutorial complete

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import ColorChangeButton from './ColorChangeButton'
+import './App.css';
+
+function App() {
+  const [color, setColor] = useState('')
+  return (
+    <div className={`react-root ${color}`} >
+      <div className="centered">
+        <h1>Color Picker</h1>
+        <ColorChangeButton color="red" setColor={setColor}/>
+        <ColorChangeButton color="blue" setColor={setColor}/>
+        <ColorChangeButton color="yellow" setColor={setColor}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
